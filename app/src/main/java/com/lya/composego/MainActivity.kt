b959c7e1bd32fb595bb48e034924891850aa3ec9
hzliyongan <hzliyongan@corp.netease.com>
2021-10-27 15:14:23 +0800

    init

--- app/src/main/java/com/lya/composego/MainActivity.kt
+++ app/src/main/java/com/lya/composego/MainActivity.kt
@@ -6,30 +6,42 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.insets.statusBarsPadding
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.lya.composego.ui.theme.ComposeGoTheme
-import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
-            ComposeGoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    MyApp()
+            ProvideWindowInsets {
+                ComposeGoTheme {
+                    val systemUiController = rememberSystemUiController()
+                    val darkIcons = MaterialTheme.colors.isLight
+                    SideEffect {
+                        systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = darkIcons)
+                    }
+
+                    // A surface container using the 'background' color from the theme
+                    Surface(color = MaterialTheme.colors.background) {
+                        MyApp()
+                    }
                 }
             }
         }
@@ -38,75 +50,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp() {
-    val shouldShowOnboarding = remember { mutableStateOf(true) }
-    if (shouldShowOnboarding.value) {
-        OnboardingScreen(onContinueClicked = { shouldShowOnboarding.value = false})
-    } else {
-        Greetings()
-    }
-}
-
-@Composable
-private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
-    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-        items(names.size) { index ->
-            Greeting(names[index])
-        }
-    }
-}
-
-@Composable
-private fun OnboardingScreen(onContinueClicked: () -> Unit) {
-    val context = LocalContext.current
-
-    Surface {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Welcome to the Basics Codelab!")
-            Button(
-                modifier = Modifier.padding(vertical = 24.dp),
-                onClick = onContinueClicked
-            ) {
-                Text("Continue")
-            }
-
-            Button(modifier = Modifier.padding(vertical = 24.dp), onClick = {
-                context.startActivity(Intent(context, LayoutsActivity::class.java))
-            }) {
-                Text("Launch layouts activity")
-            }
-        }
-    }
-}
-
-@Composable
-private fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-
-    val extraPadding by animateDpAsState(
-        if (expanded.value) 48.dp else 0.dp
-    )
-
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    Column(
+        modifier = Modifier.statusBarsPadding().fillMaxHeight(),
+        verticalArrangement = Arrangement.Center
     ) {
-        Row(modifier = Modifier.padding(24.dp).padding(bottom = extraPadding)) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(text = "Hello, ")
-                Text(text = name)
-            }
-            OutlinedButton(
-                onClick = {
-                    expanded.value = !expanded.value
-                }
-            ) {
-                Text(if (expanded.value) "Show less" else "Show more")
-            }
-        }
+        EntryItem(title = "JetNews", cls = LayoutsActivity::class.java)
+    }
+}
+
+@Composable
+private fun EntryItem(
+    modifier: Modifier = Modifier,
+    title: String,
+    cls: Class<*>
+) {
+    val context = LocalContext.current
+    Button(
+        onClick = {
+            context.startActivity(Intent(context, cls))
+        },
+        modifier.fillMaxWidth().padding(8.dp)
+    ) {
+        Text(title)
     }
 }
 
@@ -115,14 +80,5 @@ private fun Greeting(name: String) {
 fun DefaultPreview() {
     ComposeGoTheme {
         MyApp()
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnboardingPreview() {
-
-    ComposeGoTheme {
-        OnboardingScreen(onContinueClicked = {  })
     }
 }
