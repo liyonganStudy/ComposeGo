51a345f9ce025583ac2745db8b3f557448b68872
hzliyongan <hzliyongan@corp.netease.com>
2021-11-01 16:53:43 +0800

    develop jetcaster

--- app/src/main/java/com/lya/composego/LayoutsActivity.kt
+++ app/src/main/java/com/lya/composego/LayoutsActivity.kt
@@ -2,26 +2,23 @@
 
 package com.lya.composego
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.MeasurePolicy
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
@@ -32,11 +29,49 @@ class LayoutsActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeGoTheme {
-                LayoutsCodeLab()
+                ParentLayout(
+                    Modifier
+                        .size(100.dp)
+                        .padding(10.dp)
+                        .background(Color.Blue)
+                ) {
+                    ChildLayout {
+                        Box {}
+                    }
+                    ChildLayout {}
+                }
             }
         }
     }
 }
+
+@Composable
+private fun ParentLayout(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    //布局的测量策略
+    val measurePolicy = MeasurePolicy { measurables, constraints ->
+        //1.测量 children
+        val placeables = measurables.map { child ->
+            child.measure(constraints)
+        }
+
+        var xPosition = 0
+        //2.放置 children
+        layout(constraints.minWidth, constraints.minHeight) {
+            placeables.forEach { placeable ->
+                placeable.placeRelative(xPosition, 0)
+                xPosition += placeable.width
+            }
+        }
+    }
+    //代码分析入口
+    Layout(content = content, modifier = modifier, measurePolicy = measurePolicy)
+}
+
+@Composable
+private fun ChildLayout(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    //...代码和ParentLayout类似
+}
+
 
 @Composable
 private fun LayoutsCodeLab(modifier: Modifier = Modifier) {
